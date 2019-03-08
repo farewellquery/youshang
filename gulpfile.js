8dcd2e504fe9207cfdf1bef34f506950f8cce634
farewellquery <1071083581@qq.com>
2019-03-08 16:53:53 +0800

    第一次上传

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+
+gulp.task('test',function(){
+	console.log('hello,狗日的');
+})
+
+gulp.task('js',function(){
+	gulp.src('./src/js/*.js').pipe(concat("all.min.js"))
+	.pipe(uglify()).pipe(gulp.dest('./dist'));
+})
+
+gulp.task('default',function(){
+	gulp.watch(['./src/scss/*.scss'],['sass']);
+})
+
+gulp.task('sass',function(){
+	gulp.src('./src/scss/*.scss')
+	.pipe(sass())
+	.pipe(cssnano())
+	.pipe(rename({'suffix' : '.min'}))
+	.pipe(gulp.dest('./dist'));
+})
